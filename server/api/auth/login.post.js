e1faeb7d277d5f5c8f65e302bb673650426734ed
eralinkd <eralinkd@gmail.com>
2025-03-25 12:24:03 +0200

    uau

--- server/api/auth/login.post.js
+++ server/api/auth/login.post.js
@@ -2,12 +2,12 @@ import { verifyCredentials } from "~/server/services/userService";
 import { generateTokens } from "~/server/services/tokenService";
 
 export default defineEventHandler(async (event) => {
-  const sql = usePostgres()
-  const test = await sql`SELECT 1`
-  event.waitUntil(sql.end())
-  return {
-    test: test
-  }
+  // const sql = usePostgres()
+  // const test = await sql`SELECT 1`
+  // event.waitUntil(sql.end())
+  // return {
+  //   test: test
+  // }
   try {
     const { email, password } = await readBody(event);
     const user = await verifyCredentials(email, password);
